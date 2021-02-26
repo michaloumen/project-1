8d86e4da5fdb1c058002220bbb39c98aba99fbb0
michaloumen <michelle.lmendonca@gmail.com>
2021-02-26 15:23:47 -0300

    Done

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# project-1
+# Project #1
+<img src="Captura de tela de 2021-02-24 14-54-44.png" width="500">
+
+This is my first project as part of the Ironhack bootcamp. Where to play the Candy Crush game using HTML, CSS and Javascript.
+
+## Summary 
+- [Game rules](#game-rules)
+- [Installing](#installing)
+- [Acknowledgments](#acknowledgments)
+- [Credits](#credits)
+- [About me](#about-me)
+
+
+## Game Rules
+Candy Crush is a single player game where players swap pieces of colored candy on a board to make a combination of three, four or five identical colors in a row or column. It is possible to exchange a piece of place with those around it. Scoring is done using these combinations. When this happens, the combined candies are crushed and a new random combination is generated.<br>
+The objective of this game is to achieve the defined score. There are two options for game modes: Play Timer and Play Moves. In the timer, there is a time limit for making the combinations. And in moves, there is a movement limit. When this limit is exhausted, it is possible to know if you won or lost by the message that will appear on the screen.
+
+## Installing
+<p>You can run it offline by downloading/cloning the project files or you can run my online instance from: https://michaloumen.github.io/projeto-1-game/
+
+----------------------------
+## Acknowledgments 
+To **Henrique Mendes** and **Ingrid Pitta** for teaching and helping me to get here.
+
+## Credits 
+To **Ania Kubów** where I got the tutorial to start the project.
+
+## About me  
+* Michelle Mendonça 
+* Github: https://github.com/michaloumen
+* E-mail: michelle.lmendonca@gmail.com
+* Linkedin: https://www.linkedin.com/in/michelle-louren%C3%A7o-mendon%C3%A7a-856b83169/
+
---
+++ js/board.js
@@ -0,0 +1,468 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const scoreDisplay = document.getElementById("score");
+  const startBtnOne = document.querySelector("#startTimer");
+  const startBtnTwo = document.querySelector("#startMoves");
+  const timeLeftDisplay = document.querySelector("#time-left");
+  const moveLeftDisplay = document.querySelector("#move-left");
+  const width = 8; //será de 8x8
+  const squares = [];
+  let score = 0;
+  let timeLeft = 10;
+  let moveLeft = 10;
+  let points = 40;
+  let colorBeingDragged;
+  let colorBeingReplaced;
+  let squareIdBeingDragged;
+  let squareIdBeingReplaced;
+  let mode = "";
+
+  const gameSound1 = new Audio();
+  gameSound1.src = "./songs/candycrush.mp3";
+  gameSound1.volume = 0.3;
+
+  const gameSound2 = new Audio();
+  gameSound2.src = "./songs/ploc.mp3";
+  gameSound2.volume = 0.5;
+
+  const candyColors = [
+    "url(images/red-candy.png)",
+    "url(images/yellow-candy.png)",
+    "url(images/orange-candy.png)",
+    "url(images/purple-candy.png)",
+    "url(images/green-candy.png)",
+    "url(images/blue-candy.png)",
+  ];
+
+  function countDown() {
+    timeLeftDisplay.innerHTML = `Seconds left: ${timeLeft}<br><br>SCORE:`;
+    setInterval(function () {
+      if (timeLeft <= 0) {
+        clearInterval((timeLeft = 0));
+        grid.style.display = "none";
+      }
+
+      timeLeftDisplay.innerHTML = `Seconds left: ${timeLeft}<br><br>SCORE:`;
+      timeLeft -= 1;
+      if (timeLeft <= 0 || score > points) {
+        timeLeftDisplay.innerHTML = "Congrats, you won! 🎉";
+        grid.style.display = "none";
+      }
+      if (timeLeft <= 0 && score < points) {
+        timeLeftDisplay.innerHTML = "You ran out of moves 🥵";
+      }
+    }, 1000);
+  }
+
+  function countMoves() {
+    if (moveLeft > 0) {
+      timeLeftDisplay.style.display = "none";
+      moveLeftDisplay.innerHTML = `Moves Left: ${moveLeft}<br><br>SCORE:`;
+    }
+
+    if (moveLeft === 0) {
+      grid.style.display = "none";
+    }
+
+    if (moveLeft <= 0 || score > points) {
+      moveLeftDisplay.innerHTML = "Congrats, you won! 🎉";
+      grid.style.display = "none";
+    }
+
+    if (moveLeft <= 0 && score < points) {
+      moveLeftDisplay.innerHTML = "You ran out of moves 🥵";
+    }
+  }
+
+  window.onload = function () {
+    startBtnOne.addEventListener("click", countDown);
+    startBtnTwo.addEventListener("click", countMoves);
+    var startTimer = document.getElementById("startTimer");
+    var startMoves = document.getElementById("startMoves");
+    grid.style.display = "none";
+    timeLeftDisplay.innerHTML = `In Play Timer you have ${timeLeft} seconds to gain ${points} points<br><br>In Play Moves you have ${moveLeft} moves to gain ${points} points<br><br>🍬🍩 Now crush that candy! 🍩🍬`;
+
+    startTimer.onclick = function () {
+      document.getElementById("startTimer").style.display = "none";
+      document.getElementById("startMoves").style.display = "none";
+      grid.style.display = "flex";
+      mode = "timer";
+      executeGame();
+    };
+
+    startMoves.onclick = function () {
+      document.getElementById("startMoves").style.display = "none";
+      document.getElementById("startTimer").style.display = "none";
+      document.getElementById("startMoves").style.display = "none";
+      grid.style.display = "flex";
+      mode = "moves";
+      executeGame();
+    };
+
+    function executeGame() {
+      gameSound1.play();
+      function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+          const square = document.createElement("div");
+          //para fazer o mouse arrastar os doces
+          square.setAttribute("draggable", true);
+          //isso vai servir para depois conseguir arrastar as cores porque teremos 64 quadrinhos no board
+          square.setAttribute("id", i);
+          //distribui randomicamente as cores
+          let randomColor = Math.floor(Math.random() * candyColors.length);
+          //estou passando as cores randômicas para o array de 'const squares'
+          square.style.backgroundImage = candyColors[randomColor];
+          grid.appendChild(square);
+          squares.push(square);
+        }
+      }
+      createBoard();
+
+      //fazer 'dragable' com os candies. Criamos os eventos com os quais podemos interagir no elemento arrastável: dragstart, gradend, dragover...
+
+      squares.forEach((square) =>
+        square.addEventListener("dragstart", dragStart)
+      );
+      squares.forEach((square) => square.addEventListener("dragend", dragEnd));
+      squares.forEach((square) =>
+        square.addEventListener("dragover", dragOver)
+      );
+      squares.forEach((square) =>
+        square.addEventListener("dragenter", dragEnter)
+      );
+      squares.forEach((square) =>
+        square.addEventListener("dragleave", dragLeave)
+      );
+      squares.forEach((square) => square.addEventListener("drop", dragDrop));
+
+      function dragStart() {
+        //definir a cor
+        colorBeingDragged = this.style.backgroundImage;
+        //definir o que será arrastado. Colocar parseInt porque tem que ser um número
+        squareIdBeingDragged = parseInt(this.id);
+        console.log(colorBeingDragged);
+        console.log(this.id, "dragstart");
+        if (mode === "moves") {
+          moveLeft -= 1;
+          countMoves();
+        }
+      }
+
+      //'e' para 'event'
+      //preventDefault --- cancela o evento se for cancelável sem parar a sua propagação --- importante para não dar erro na hora de deslizar o quadradinho de lugar
+      function dragOver(e) {
+        e.preventDefault();
+        console.log(this.id, "dragover");
+      }
+
+      function dragEnter(e) {
+        e.preventDefault();
+        console.log(this.id, "dragenter");
+      }
+
+      function dragLeave() {
+        console.log(this.id, "dragleave");
+        gameSound2.play();
+      }
+
+      function dragDrop() {
+        console.log(this.id, "dragdrog");
+        colorBeingReplaced = this.style.backgroundImage;
+        squareIdBeingReplaced = parseInt(this.id);
+        //fazer os dois quadrados trocarem de cor quando arrastamos uma cor para o lado
+        this.style.backgroundImage = colorBeingDragged;
+        //a cor que vai ficar
+        squares[
+          squareIdBeingDragged
+        ].style.backgroundImage = colorBeingReplaced;
+      }
+
+      function dragEnd() {
+        console.log(this.id, "dragend");
+        //qual é um movimento válido?
+        let validMoves = [
+          //-1 porque temos trocar. Dizemos que o número do quadrado é 67, e vamos trocar para a posição 66 e depois mexe no widt. Depois faz isso com um array imediatamente acima desse e diminui o width -- não entendi direito porque
+          //o que eu entendi é que em um código estamos fazendo movimento de trocar para direito e no outro o movimento de trocar para a esquerda
+          squareIdBeingDragged - 1,
+          squareIdBeingDragged - width,
+          squareIdBeingDragged + 1,
+          squareIdBeingDragged + width,
+        ];
+        //se o número passado for válido, ele será incluído no array
+        let validMove = validMoves.includes(squareIdBeingReplaced);
+
+        if (squareIdBeingReplaced && validMove) {
+          //se for verdadeiro, faz a troca e está pronto para limpar (null) e começar uma troca de cores nova
+          squareIdBeingReplaced = null;
+          //se é um quadrado que pode ser trocar de lugar mas não estamos tentando fazer um movimento válido, o quadrado deve permanecer em sua posição original. E isso tem que ser feito para o quadrado que queremos trocar e para o que será trocado -- por isso 2x o [squareIdBeingReplaced]
+        } else if (squareIdBeingReplaced && !validMove) {
+          squares[
+            squareIdBeingReplaced
+          ].style.backgroundImage = colorBeingReplaced;
+          squares[
+            squareIdBeingDragged
+          ].style.backgroundImage = colorBeingDragged;
+          //e se nada disso acontecer, não é um quadrado que pode trocar de lugar e nem é um movimento válido, eles só vão ficar parados mesmo
+        } else
+          squares[
+            squareIdBeingDragged
+          ].style.backgroundImage = colorBeingDragged;
+      }
+
+      //essa função faz os espaços brancos sumirem e fazer os doces que estão acima deles cairem
+      function moveDown() {
+        for (i = 0; i < 55; i++) {
+          if (squares[i + width].style.backgroundImage === "") {
+            squares[i + width].style.backgroundImage =
+              squares[i].style.backgroundImage;
+            squares[i].style.backgroundImage = "";
+            //aqui é para completar esse espaço que ficou vazio com outras bolinhas coloridas randomicamente **tá dando uns bugs que as vezes não completa os espaços
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+            const isFistRow = firstRow.includes(i);
+            if (isFistRow && squares[i].style.backgroundImage === "") {
+              let randomColor = Math.floor(Math.random() * candyColors.length);
+              squares[i].style.backgroundImage = candyColors[randomColor];
+            }
+          }
+        }
+      }
+
+      //para combinações de 5
+      function checkRowForFive() {
+        for (i = 0; i < 59; i++) {
+          let rowOfFive = [i, i + 1, i + 2, i + 3, i + 4];
+          let decidedColor = squares[i].style.backgroundImage;
+          const isBlank = squares[i].style.backgroundImage === "";
+
+          const notValid = [
+            4,
+            5,
+            6,
+            7,
+            12,
+            13,
+            14,
+            15,
+            20,
+            21,
+            22,
+            23,
+            28,
+            29,
+            30,
+            31,
+            36,
+            37,
+            38,
+            39,
+            44,
+            45,
+            46,
+            47,
+            52,
+            53,
+            54,
+            55,
+          ];
+          if (notValid.includes(i)) continue;
+
+          if (
+            rowOfFive.every(
+              (index) =>
+                squares[index].style.backgroundImage === decidedColor &&
+                !isBlank
+            )
+          ) {
+            score += 5;
+            scoreDisplay.innerHTML = score;
+            rowOfFive.forEach((index) => {
+              squares[index].style.backgroundImage = "";
+            });
+          }
+        }
+      }
+      checkRowForFive();
+
+      function checkColumnForFive() {
+        for (i = 0; i < 47; i++) {
+          let columnOfFive = [
+            i,
+            i + width,
+            i + width * 2,
+            i + width * 3,
+            i + width * 4,
+          ];
+          let decidedColor = squares[i].style.backgroundImage;
+          const isBlank = squares[i].style.backgroundImage === "";
+
+          if (
+            columnOfFive.every(
+              (index) =>
+                squares[index].backgroundImage === decidedColor && !isBlank
+            )
+          ) {
+            score += 5;
+            scoreDisplay.innerHTML = score;
+            columnOfFive.forEach((index) => {
+              squares[index].style.backgroundImage = "";
+            });
+          }
+        }
+      }
+      checkColumnForFive();
+
+      //para combinações de 4
+      function checkRowForFour() {
+        for (i = 0; i < 60; i++) {
+          let rowOfFour = [i, i + 1, i + 2, i + 3];
+          let decidedColor = squares[i].style.backgroundImage;
+          const isBlank = squares[i].style.backgroundImage === "";
+
+          const notValid = [
+            5,
+            6,
+            7,
+            13,
+            14,
+            15,
+            21,
+            22,
+            23,
+            29,
+            30,
+            31,
+            37,
+            38,
+            39,
+            45,
+            46,
+            47,
+            53,
+            54,
+            55,
+          ];
+          if (notValid.includes(i)) continue;
+
+          if (
+            rowOfFour.every(
+              (index) =>
+                squares[index].style.backgroundImage === decidedColor &&
+                !isBlank
+            )
+          ) {
+            score += 4;
+            scoreDisplay.innerHTML = score;
+            rowOfFour.forEach((index) => {
+              squares[index].style.backgroundImage = "";
+            });
+          }
+        }
+      }
+      checkRowForFour();
+
+      function checkColumnForFour() {
+        for (i = 0; i < 47; i++) {
+          let columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+          let decidedColor = squares[i].style.backgroundImage;
+          const isBlank = squares[i].style.backgroundImage === "";
+
+          if (
+            columnOfFour.every(
+              (index) =>
+                squares[index].backgroundImage === decidedColor && !isBlank
+            )
+          ) {
+            score += 4;
+            scoreDisplay.innerHTML = score;
+            columnOfFour.forEach((index) => {
+              squares[index].style.backgroundImage = "";
+            });
+          }
+        }
+      }
+      checkColumnForFour();
+
+      //procurando matches de 3 peças
+      //começa com um loop de 61 peças porque o jogo tem 64 -- de 0 a 63 e se não desconta 3, vai dar erro porque vai faltar combinações
+      function checkRowForThree() {
+        for (i = 0; i < 61; i++) {
+          let rowOfThree = [i, i + 1, i + 2];
+          //para quando arrastamos a cor, tem que fazer a combinação de 3 de acordo com aquela cor que acabamos de arrastar
+          let decidedColor = squares[i].style.backgroundImage;
+          //blank para quando estiver vazio for true (=== '')
+          const isBlank = squares[i].style.backgroundImage === "";
+
+          //só que ele está contando 3 cores juntas mesmo quando começa de um lado do board e termina do outro. Isso não é uma combinação válida. Precisamos excluir. Vamos escrever todas as combinações de índice que não quero que minha linha comece porque vai quebrar e terminar do outro lado
+          const notValid = [
+            6,
+            7,
+            14,
+            15,
+            22,
+            23,
+            30,
+            31,
+            38,
+            39,
+            46,
+            47,
+            54,
+            55,
+          ];
+          if (notValid.includes(i)) continue; //se o número for qualquer um desses, vamos pular ele
+
+          if (
+            rowOfThree.every(
+              (index) =>
+                squares[index].style.backgroundImage === decidedColor &&
+                !isBlank
+            )
+          ) {
+            score += 3;
+            scoreDisplay.innerHTML = score;
+            rowOfThree.forEach((index) => {
+              //se encontrar uma correspondência, quero pegar as linhas de três arrays e dar a ela uma cor de fundo vazia
+              squares[index].style.backgroundImage = "";
+            });
+          }
+        }
+      }
+      checkRowForThree();
+
+      function checkColumnForThree() {
+        //47 porque é o número da nossa última coluna
+        for (i = 0; i < 47; i++) {
+          let columnOfThree = [i, i + width, i + width * 2];
+          let decidedColor = squares[i].style.backgroundImage;
+          const isBlank = squares[i].style.backgroundImage === "";
+
+          if (
+            columnOfThree.every(
+              (index) =>
+                squares[index].style.backgroundImage === decidedColor &&
+                !isBlank
+            )
+          ) {
+            score += 3;
+            scoreDisplay.innerHTML = score;
+            columnOfThree.forEach((index) => {
+              squares[index].style.backgroundImage = "";
+            });
+          }
+        }
+      }
+      checkColumnForThree();
+
+      //mas isso de apagar as 3 linhas só acontece quando inicia o navegador. Durante o jogo isso não acontecerá mais e não é para ser assim. Por isso, vamos usar o setInterval
+      window.setInterval(function () {
+        moveDown();
+        checkRowForFive();
+        checkColumnForFive();
+        checkRowForFour();
+        checkColumnForFour();
+        checkRowForThree();
+        checkColumnForThree();
+      }, 100);
+    }
+  };
+});
